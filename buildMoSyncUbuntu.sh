d1589c3f65a0e0bc8f1a8a31301eaa75740cad79
sixman9 <sixman9@gmail.com>
2013-05-19 03:43:44 +0200

    Added 95%+ of MoSync GCC build algo (needs patching!), also outline of 64-bit MoSync IDE build branch.

--- buildMoSyncUbuntu.sh
+++ buildMoSyncUbuntu.sh
@@ -5,6 +5,8 @@
 mosyncHomePage="http://www.mosync.com"
 mosyncNightly="/nightly-builds"
 
+mosyncGCCGitProjName=gcc_trunk
+
 mosyncIDEGitURL="git://github.com/MoSync/MoSync.git"
 
 #Install any tools we need
@@ -13,7 +15,17 @@ mosyncIDEGitURL="git://github.com/MoSync/MoSync.git"
 #Determine the OS architecture (32/64bit) using 'uname -m', then run the required commands (see http://stackoverflow.com/a/106416/304330)
 ourArch=$(uname -m)
 
-#latestMoSyncNightlyBundleURL <nightly-page-url> <file-suffix>
+#Various build and install directory definitions
+baseDir=/tmp
+gccBuildDir="$baseDir"/mosyncBuild/gcc
+mosyncBuildDir="$baseDir"/mosyncBuild/mosync
+eclipeBuildDir="$baseDir"/mosyncBuild/eclipse
+installDir="$HOME"/mosync
+installBinDir=$installDir/bin
+installGCCDir=$installDir/libexec/gcc/mapip/3.4.6
+
+
+#Function to determine latestMoSyncNightlyBundleURL <nightly-page-url> <file-suffix>
 function latestMoSyncNightlyBundleURL() {
 	local mosyncNightlyURL=$1
 	local mosyncBundleSuffix=$2
@@ -55,13 +67,51 @@ latestNightlyBundleURL=$(latestMoSyncNightlyBundleURL "$mosyncHomePage$mosyncNig
 
 #echo $latestNightlyBundleURL
 
-#List remote Git branches
+#---Start MoSync GCC build
+
+#Create MoSync Install/Build directories - See http://www.mosync.com/documentation/manualpages/building-mosync-source-linux
+mkdir -p "$installBinDir"
+mkdir -p "$installGCCDir"
+
+export MOSYNCDIR="$installDir"
+
+#Let's build MoSync GCC from GitHub
+mkdir -p "$gccBuildDir"
+pushd "$gccBuildDir"
+git clone git://github.com/MoSync/gcc.git "$mosyncGCCGitProjName"
+
+pushd ./"$mosyncGCCGitProjName"
+#***TASK -> APPLY GCC PATCH!!!***
+./configure-linux.sh
+pushd build/gcc
+make
+
+#Move MoSync's built gcc etc. to the MoSync install
+#Compilation may have failed when trying to build libgcc - this *is* OK - for the build scripts to find MoSync GCC it has to be moved to the installation directory
+cp gcc/xgcc gcc/cpp $MOSYNCDIR/bin
+cp gcc/cc1 gcc/cc1plus $MOSYNCDIR/libexec/gcc/mapip/3.4.6/
+
+#pop all dirs
+dirs -c
+
+#---End MoSync GCC build 
+
+#Gather a list of MoSync's remote Git branches
 mosyncBranches=$(git ls-remote "$mosyncIDEGitURL" | grep -i heads | awk -F'/' '{print $NF}')
 
 select chosenBranch in $mosyncBranches;
 do
 	echo You chose $chosenBranch
 	break
+	
+	if [ $ourArch == 'x86_64' ]; then
+	  # 64-bit tasks here
+	  #Apply 64-bit patch to mosync (https://github.com/fredrikeldh/Eclipse/commit/c059d516e0e89ed4308f27cdc03229ec01fde740)
+	  #See http://blog.mhartl.com/2008/07/01/using-git-to-pull-in-a-patch-from-a-single-commit
+	
+	else
+	  # 32-bit tasks here
+fi
 done
 
 
