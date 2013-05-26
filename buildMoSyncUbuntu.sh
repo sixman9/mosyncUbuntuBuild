eca47862c2b078344a8efe04a5f9aea62e155569
sixman9 <sixman9@gmail.com>
2013-05-26 02:54:53 +0100

    Rough MoSync tools nightly build implmented, slight script refactoring. NB - some functions deactivated for test purposes.

--- buildMoSyncUbuntu.sh
+++ buildMoSyncUbuntu.sh
@@ -11,9 +11,6 @@ mosyncGCCGitProjName=mosyn_gcc
 
 mosyncIDEGitURL="git://github.com/MoSync/MoSync.git"
 
-#Install any tools we need
-sudo apt-get install gcc g++ bison flex ruby rake subversion rpm libgtk2.0-dev libexpat1-dev libbluetooth3-dev libsdl1.2-dev libsdl-image1.2-dev libsdl-ttf2.0-dev libfreeimage-dev gperf libssl-dev git p7zip-full html-xml-utils libwebkitgtk-1.0-0 build-essential 
-
 #Determine the OS architecture (32/64bit) using 'uname -m', then run the required commands (see http://stackoverflow.com/a/106416/304330)
 ourArch=$(uname -m)
 
@@ -29,8 +26,8 @@ installGCCDir=$mosyncDir/libexec/gcc/mapip/3.4.6
 selectedMoSyncSDKBranch="ThreeTwoOne"
 
 
-#Function to determine latestMoSyncNightlyBundleURL <nightly-page-url> <file-suffix>
-function funcLatestMoSyncNightlyBundleURL() {
+#Function to determine FindLatestMoSyncNightlyBundleURL <nightly-page-url> <file-suffix>
+function funcFindLatestMoSyncNightlyBundleURL() {
 	local mosyncNightlyURL=$1
 	local mosyncBundleSuffix=$2
 
@@ -64,21 +61,39 @@ function funcLatestMoSyncNightlyBundleURL() {
 	echo $resolvedMosyncNightlyURL
 }
 
+function funcInit() {
+
+	#Install any tools we need
+	sudo apt-get install gcc g++ bison flex ruby rake subversion rpm libgtk2.0-dev libexpat1-dev libbluetooth3-dev libsdl1.2-dev libsdl-image1.2-dev libsdl-ttf2.0-dev libfreeimage-dev gperf libssl-dev git p7zip-full html-xml-utils libwebkitgtk-1.0-0 build-essential 
+
+}
+
+function funcInitDirs() {
+
+	#pop all dirs
+	dirs -c
+
+	#Create MoSync Install/Build directories - See http://www.mosync.com/documentation/manualpages/building-mosync-source-linux	
+	mkdir -p "$gccBuildDir"
+	mkdir -p "$installBinDir"
+	mkdir -p "$installGCCDir"
+	mkdir -p "$mosyncBuildDir"
+	mkdir -p "$eclipeBuildDir"
+
+	export MOSYNCDIR="$mosyncDir"
+}
+
+function funcDownloadGCCSrc() {
+
+	pushd "$gccBuildDir"
+	git clone git://github.com/MoSync/gcc.git "$mosyncGCCGitProjName"
+}
+
 function funcBuildGCC() {
 	#---Start MoSync GCC build
 
-	#Create MoSync Install/Build directories - See http://www.mosync.com/documentation/manualpages/building-mosync-source-linux
-	mkdir -p "$installBinDir"
-	mkdir -p "$installGCCDir"
-
-	export MOSYNCDIR="$mosyncDir"
-
 	#Let's build MoSync GCC from GitHub
-	mkdir -p "$gccBuildDir"
-	pushd "$gccBuildDir"
-	git clone git://github.com/MoSync/gcc.git "$mosyncGCCGitProjName"
-
-	pushd ./"$mosyncGCCGitProjName"
+	pushd "$gccBuildDir"/"$mosyncGCCGitProjName"
 	#APPLY GCC PATCH (re: Sudarais)
 	patch -p1 < "$SCRIPT_DIR"/patches/gcc_patch.txt
 	./configure-linux.sh
@@ -90,44 +105,50 @@ function funcBuildGCC() {
 	cp gcc/xgcc gcc/cpp $MOSYNCDIR/bin
 	cp gcc/cc1 gcc/cc1plus $MOSYNCDIR/libexec/gcc/mapip/3.4.6/
 
-	#pop all dirs
-	dirs -c
-
 	#---End MoSync GCC build 
 }
 
 function funcBuildMoSyncTools() {
+
 	# Download mosync nightly build and extract mosync code to $MOSYNCDIR/src/mosync_trunk
 	# Apply mosync patch	
 
 	#Download and build nightly version of MoSync/Eclipse
 
 	
-	git clone git://github.com/MoSync/MoSync.git mosync_trunk
+	#git clone git://github.com/MoSync/MoSync.git mosync_trunk
 
 	#echo "Now we need to 'wget -c' and 'tar xjzf' $latestLinuxNightlyBundleURL
-	pushd "$installDir"
+	pushd "$mosyncBuildDir"
 	wget -c "$latestLinuxNightlyBundleURL"
 
-	#decompress the binary
-	tar xjzf "$latestLinuxNightlyBundleURL" 
+	#decompress the nightly, get the filename from the nightly URL
+	local filename=$(echo "$latestLinuxNightlyBundleURL" | awk -F'/' '{print $NF}')
+	echo "$filename"
+	tar xjf ./"$filename" 
 
-	push "$mosyncSrcDir"/mosync-trunk/
+	pushd "$mosyncBuildDir"/MoSync-trunk/
+
+	#APPLY SDK PATCH (re: Sudarais)
+	patch -p1 < "$SCRIPT_DIR"/patches/mosync_patch.txt
 
 	./workfile.rb CONFIG="debug"
 
 	./workfile.rb CONFIG=""
 
-	if [ $ourArch == 'x86_64' ]; then
+	#if [ $ourArch == 'x86_64' ]; then
 		# Any 64-bit tasks here
 		# Any 32-bit tasks here
-		echo about to build MoSync tools
-	fi
+	#	echo about to build MoSync tools
+	#fi
 
-	#APPLY SDK PATCH (re: Sudarais)
-	#patch -p1 < "$SCRIPT_DIR"/patches/mosync_patch.txt
+	
 
 	#Continue standard/outlined MoSync-SDK-on-Ubuntu build steps
+
+}
+
+function funcCleanUp() {
 
 	#pop all dirs
 	dirs -c
@@ -145,17 +166,22 @@ function funcBuildMoSyncEclipse() {
 }
 
 #Resolve latest MoSync Linux (.b2z, sdk src code) and Windows (EXE, to extract profiles etc.) Nightly bundles
-#bundle extension are '.exe'=>Windows, '.b2z'=>Linux '.dmg'=>Mac
-latestWindowsNightlyBundleURL=$(funcLatestMoSyncNightlyBundleURL "$mosyncHomePage$mosyncNightly" ".exe")
-latestLinuxNightlyBundleURL=$(funcLatestMoSyncNightlyBundleURL "$mosyncHomePage$mosyncNightly" ".b2z")
+#bundle extension are '.exe'=>Windows, '.bz2'=>Linux '.dmg'=>Mac
+latestWindowsNightlyBundleURL=$(funcFindLatestMoSyncNightlyBundleURL "$mosyncHomePage$mosyncNightly" ".exe")
+latestLinuxNightlyBundleURL=$(funcFindLatestMoSyncNightlyBundleURL "$mosyncHomePage$mosyncNightly" ".bz2")
 
 #echo $latestWindowsNightlyBundleURL
 #echo $latestLinuxNightlyBundleURL
+#exit 0
 
 #Build MoSync GCC/SDK/Eclipse - call various functions
-funcBuildGCC
+#funcInit
+funcInitDirs
+#funcDownloadGCCSrc
+#funcBuildGCC
 funcBuildMoSyncTools
-funcBuildMoSyncEclipse
+#funcBuildMoSyncEclipse
+funcCleanUp
 
 
 
