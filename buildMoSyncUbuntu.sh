600a06853196e0793807deb2f78335093923b13f
sixman9 <sixman9@gmail.com>
2013-10-01 17:00:56 +0000

    Corrected Bash 'IF' logic when evaluating true/false strings.

--- buildMoSyncUbuntu.sh
+++ buildMoSyncUbuntu.sh
@@ -33,9 +33,9 @@ eclipeBuildDir="$mosyncSrcDir"/eclipse
 installBinDir=$mosyncDir/bin
 installGCCDir=$mosyncDir/libexec/gcc/mapip/3.4.6
 
-selectedMoSyncSDKBranch="ThreeTwoOne"
+selectedMoSyncSDKBranch="ThreeThreeOne"
 
-toolsBuildUseGit=false
+toolsBuildUseGit="false"
 
 
 #Function to determine FindLatestMoSyncNightlyBundleURL <nightly-page-url> <file-suffix>
@@ -130,8 +130,16 @@ function funcBuildMoSyncTools() {
 
 	pushd "$mosyncBuildDir"
 
-	if [ ! "$toolsBuildUseGit" ]
-	then 
+	if [ "$toolsBuildUseGit" eq "true" ]
+	then
+		#Use the git-based tools repo
+		if [ ! -d "$mosyncBuildDir"/MoSync-trunk-git ]
+		then
+			git clone git://github.com/MoSync/MoSync.git MoSync-trunk-git
+		fi
+
+		pushd "$mosyncBuildDir"/MoSync-trunk-git/ 
+	else
 		#Download and build nightly version of MoSync/Eclipse
 		#echo "Now we need to 'wget -c' and 'tar xjzf' $latestLinuxNightlyBundleURL
 		wget -c "$latestLinuxNightlyBundleURL"
@@ -142,14 +150,6 @@ function funcBuildMoSyncTools() {
 		tar xjf ./"$filename"
 
 		pushd "$mosyncBuildDir"/MoSync-trunk/
-	else
-		#Use the git-based tools repo
-		if [ ! -d "$mosyncBuildDir"/MoSync-trunk-git ]
-		then
-			git clone git://github.com/MoSync/MoSync.git MoSync-trunk-git
-		fi
-
-		pushd "$mosyncBuildDir"/MoSync-trunk-git/
 	fi
 
 	#APPLY SDK PATCH (re: Sudarais)
