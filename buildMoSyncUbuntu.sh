0cf410514173e6f1a1eea9bd6993e6067ad632ba
sixman9 <sixman9@gmail.com>
2013-10-10 17:08:18 +0100

    Corrected true/false usage, altered remote git patch URL

--- buildMoSyncUbuntu.sh
+++ buildMoSyncUbuntu.sh
@@ -25,18 +25,74 @@ fi
 mosyncIDEGitURL="git://github.com/MoSync/MoSync.git"
 
 #Various build and install directory definitions
-mosyncDir="$HOME"/mosync
-mosyncSrcDir="$mosyncDir"/src
+installBaseDir="$HOME"
+mosyncDirName=Mosync
+eclipseDirName=eclipse
+export MOSYNCDIR="$installBaseDir"/"$mosyncDirName"
+
+mosyncSrcDir=/tmp/"$mosyncDirName"/src
 gccBuildDir="$mosyncSrcDir"/gcc
-mosyncBuildDir="$mosyncSrcDir"/mosync
-eclipeBuildDir="$mosyncSrcDir"/eclipse
-installBinDir=$mosyncDir/bin
-installGCCDir=$mosyncDir/libexec/gcc/mapip/3.4.6
+installBinDir=$MOSYNCDIR/bin
+installGCCDir=$MOSYNCDIR/libexec/gcc/mapip/3.4.6
 
-selectedMoSyncSDKBranch="ThreeThreeOne"
+selectedMoSyncSDKGitBranch="ThreeThreeOne"
 
-toolsBuildUseGit="false"
+toolsBuildUseGit=true
 
+
+function funcInstallTools() {
+
+	#Install any tools we need
+
+	if [ -f /var/lib/dpkg/lock ]
+	then
+		echo "dpkg/apt database maybe locked or in use - please check/remove the '/var/lib/dpkg/lock' file"
+		exit 1
+	fi
+
+	echo "updating apt software library references"
+	#sudo apt-get update > /dev/null
+	sudo apt-get update
+	sudo apt-get -f install
+
+	sudo apt-get --assume-yes install curl
+	sudo apt-get --assume-yes install gcc g++
+	sudo apt-get --assume-yes install bison flex ruby rake subversion rpm libgtk2.0-dev libexpat1-dev libbluetooth3-dev libsdl1.2-dev libsdl-image1.2-dev libsdl-ttf2.0-dev libfreeimage-dev gperf libssl-dev git p7zip-full html-xml-utils libwebkitgtk-1.0-0 build-essential libglew1.6-dev lib32z1-dev zlib1g-dev:i386
+
+}
+
+#Run the Tools install, early
+#funcInstallTools
+
+function funcDownloadMosyncTools() {
+	#Build Mosync tools, either from the nigtlys or git
+
+	#Install Mosync main/tools
+	pushd "$installBaseDir"
+
+	if $toolsBuildUseGit
+	then
+		#Use the git-based tools repo
+		if [ ! -d "$MOSYNCDIR" ]
+		then
+			#Get the selectedMoSyncSDKGitBranch for main Mosync tools project
+			git clone -b $selectedMoSyncSDKGitBranch "$mosyncIDEGitURL" "$mosyncDirName"
+		fi
+	else
+		#Download and build nightly version of MoSync/Eclipse
+		#echo "Now we need to 'wget -c' and 'tar xjzf' $latestLinuxNightlyBundleURL
+		wget -c "$latestLinuxNightlyBundleURL"
+
+		#decompress the nightly, get the filename from the nightly URL
+		local filename=$(echo "$latestLinuxNightlyBundleURL" | awk -F'/' '{print $NF}')
+		#echo "$filename"
+		tar xjf ./"$filename"
+
+		#Rename the untarred directories
+		mv ./MoSync-trunk ./"$mosyncDirName"
+		mv ./Eclipse "$MOSYNCDIR"/"$eclipseDirName"
+	fi
+}
 
 #Function to determine FindLatestMoSyncNightlyBundleURL <nightly-page-url> <file-suffix>
 function funcFindLatestMoSyncNightlyBundleURL() {
@@ -75,9 +131,8 @@ function funcFindLatestMoSyncNightlyBundleURL() {
 
 function funcInit() {
 
-	#Install any tools we need
-	sudo apt-get install gcc g++ bison flex ruby rake subversion rpm libgtk2.0-dev libexpat1-dev libbluetooth3-dev libsdl1.2-dev libsdl-image1.2-dev libsdl-ttf2.0-dev libfreeimage-dev gperf libssl-dev git p7zip-full html-xml-utils libwebkitgtk-1.0-0 build-essential libglew1.6-dev lib32z1-dev zlib1g-dev:i386
-
+	#Do any initial config (other than software/package installs)
+	echo
 }
 
 function funcInitDirs() {
@@ -89,10 +144,6 @@ function funcInitDirs() {
 	mkdir -p "$gccBuildDir"
 	mkdir -p "$installBinDir"
 	mkdir -p "$installGCCDir"
-	mkdir -p "$mosyncBuildDir"
-	mkdir -p "$eclipeBuildDir"
-
-	export MOSYNCDIR="$mosyncDir"
 }
 
 function funcBuildGCC() {
@@ -109,48 +160,27 @@ function funcBuildGCC() {
 
 	#Let's update and build MoSync GCC from GitHub
 	pushd "$gccBuildDir"/"$mosyncGCCGitProjDirName"
+
 	git pull
 
 	#APPLY GCC PATCH (re: Sudarais)
 	patch -p1 < "$SCRIPT_DIR"/patches/gcc_patch.txt
 	./configure-linux.sh
 	pushd build/gcc
+
 	make
 
 	#Move MoSync's built gcc etc. to the MoSync install
 	#Compilation may have failed when trying to build libgcc - this *is* OK - for the build scripts to find MoSync GCC it has to be moved to the installation directory
-	cp gcc/xgcc gcc/cpp $MOSYNCDIR/bin
-	cp gcc/cc1 gcc/cc1plus $MOSYNCDIR/libexec/gcc/mapip/3.4.6/
+	cp gcc/xgcc gcc/cpp "$installBinDir"
+	cp gcc/cc1 gcc/cc1plus "$installGCCDir"
 
 	#---End MoSync GCC build 
 }
 
 function funcBuildMoSyncTools() {
 	#Build Mosync tools, either from the nigtlys or git
-
-	pushd "$mosyncBuildDir"
-
-	if [ "$toolsBuildUseGit" eq "true" ]
-	then
-		#Use the git-based tools repo
-		if [ ! -d "$mosyncBuildDir"/MoSync-trunk-git ]
-		then
-			git clone git://github.com/MoSync/MoSync.git MoSync-trunk-git
-		fi
-
-		pushd "$mosyncBuildDir"/MoSync-trunk-git/ 
-	else
-		#Download and build nightly version of MoSync/Eclipse
-		#echo "Now we need to 'wget -c' and 'tar xjzf' $latestLinuxNightlyBundleURL
-		wget -c "$latestLinuxNightlyBundleURL"
-
-		#decompress the nightly, get the filename from the nightly URL
-		local filename=$(echo "$latestLinuxNightlyBundleURL" | awk -F'/' '{print $NF}')
-		#echo "$filename"
-		tar xjf ./"$filename"
-
-		pushd "$mosyncBuildDir"/MoSync-trunk/
-	fi
+	pushd "$MOSYNCDIR"
 
 	#APPLY SDK PATCH (re: Sudarais)
 	patch -p1 < "$SCRIPT_DIR"/patches/mosync_patch.txt
@@ -171,21 +201,18 @@ function funcBuildMoSyncTools() {
 
 }
 
-function funcCleanUp() {
-
-	#pop all dirs
-	dirs -c
-}
-
 function funcBuildMoSyncEclipse() {
 
-	pushd "mosyncSrcDir"
+	pushd "$MOSYNCDIR"
 
-	#Git clone the required Eclipse MoSync branch
-	#git clone -b ThreeTwoOne git://github.com/MoSync/Eclipse.git
-	git clone -b ThreeThreeOne git://github.com/MoSync/Eclipse.git
+	if $toolsBuildUseGit
+	then
+		#Git clone the required Eclipse MoSync branch
+		git clone -b $selectedMoSyncSDKGitBranch git://github.com/MoSync/Eclipse.git $eclipseDirName
+	fi
 
-	pushd Eclipse
+	#Change to the eclipse directory
+	pushd ./"$eclipseDirName"
 
 	if [ $ourArch == 'x86_64' ]; then
 		#Any 64-bit tasks here
@@ -193,7 +220,7 @@ function funcBuildMoSyncEclipse() {
 		#See http://blog.mhartl.com/2008/07/01/using-git-to-pull-in-a-patch-from-a-single-commit
 
 		#Add the Git remote we want the patch from, the update/fetch our local info about it
-		git add remote fredrikeldh https://github.com/fredrikeldh/Eclipse
+		git remote add fredrikeldh git://github.com/fredrikeldh/Eclipse.git
 		git fetch fredrikeldh
 
 		git cherry-pick c059d516e0e89ed4308f27cdc03229ec01fde740
@@ -204,14 +231,25 @@ function funcBuildMoSyncEclipse() {
 
 	#Download http://www.mosync.com/down/target-platform.zip to directory 'com.mobilesorcery.sdk.product/build'
 	pushd com.mobilesorcery.sdk.product/build
-	wget http://www.mosync.com/down/target-platform.zip
+
+	wget -c http://www.mosync.com/down/target-platform.zip
 
 	#ant build (java-make equivalent) MoSync-Eclipse
 	ant release
 
 	#Move to the ant build's output directory, then make the Eclipse binary executable
 	pushd buildresult/I.MoSync/MoSync-linux.gtk.x86-unzipped/mosync
+
  	chmod +x mosync
+}
+
+function funcCleanUp() {
+
+	#pop all dirs
+	dirs -c
+
+	#Open the MoSync install directory in file manager window
+	xdg-open "$MOSYNCDIR"
 }
 
 #Resolve latest MoSync Linux (.b2z, sdk src code) and Windows (EXE, to extract profiles etc.) Nightly bundles
@@ -224,7 +262,9 @@ latestLinuxNightlyBundleURL=$(funcFindLatestMoSyncNightlyBundleURL "$mosyncHomeP
 #exit 0
 
 #Build MoSync GCC/SDK/Eclipse - call various functions
+#We ran funcInstallTools() earlier
 funcInit
+funcDownloadMosyncTools
 funcInitDirs
 funcBuildGCC
 funcBuildMoSyncTools
